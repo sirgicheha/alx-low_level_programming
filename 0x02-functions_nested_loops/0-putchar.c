c15ceaa1ca7d812a3dafde3808ec6cffbfd84e8e
sirgicheha <samuelgicheha03@gmail.com>
2023-02-21 01:43:04 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+   _putchar("_");
+   _putchar("p");
+   _putchar("u");
+   _putchar("t");
+   _putchar("c");
+   _putchar("h");
+   _putchar("a");
+   _putchar("r");
+
+   return 0;
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+nested loops task c language
